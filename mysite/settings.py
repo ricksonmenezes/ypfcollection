0838986fe13f2cbf5f013b601c6e01d38c854384
rickson <ricksonmenezes@gmail.com>
2018-03-23 17:37:11 +0530

    after removing polls from settings and url

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'ypfcollection.apps.YpfcollectionConfig',
     # 'polls.apps.PollsConfig',
-	'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
