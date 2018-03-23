b8b5446b763f608398287cf2776bedfb397a1be1
rickson <ricksonmenezes@gmail.com>
2018-03-23 17:34:46 +0530

    after removing polls from settings and url

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    # path('polls/', include('polls.urls')),
     path('ypf/', include('ypfcollection.urls')),
 	path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
